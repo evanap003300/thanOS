29c705f2856f446634964863bb1431b5ec80b27a
Evan Phillips <evanap003300@gmail.com>
2026-01-06 15:45:10 -0500

    wrote inital gdt.h

---
+++ src/gdt.h
@@ -0,0 +1,29 @@
+#pragma once
+#include <stdint.h>
+
+struct __attribute__((packed)) GDTENTRY {
+	uint16_t limit_low;
+	uint16_t base_low;
+	uint8_t base_middle;
+	uint8_t access;
+	uint8_t granularity;
+	uint8_t base_high;
+};
+
+struct __attribute__((packed)) GDTDescriptor {
+	uint16_t size;
+	uint64_t offset;
+};
+
+class GDT {
+	public:
+		// 0: Null Descriptor
+		// 1: Kernel code (ring 0)
+		// 2: Kernel data (ring 0)
+		// ... add other rings later for user
+		GDTEntry gdt_entries[3];
+		GDTDescriptor gdt_descriptor;
+		void init();
+};
+
+extern "C" void load_gdt(uint64_t gdt_descriptor_address);
