eca66ca009acd24686f9c79ed5e32287c0efd5ea
Evan Phillips <evanap003300@gmail.com>
2026-01-06 21:03:13 -0500

    started idt

---
+++ src/idt.cpp
@@ -0,0 +1,31 @@
+#include <int.h>
+
+IDTEntry IDT::idt[256];
+IDTR IDT::idtr;
+
+extern "C" void load_idt(uint64_t idtr_addr);
+
+void IDT::set_gate(uint8_t vector, void* isr, uint8_t flags) {
+	uint64_t addr = (uint64_t)isr;
+
+	IDTEntry* entry = &idt[vector];
+
+	entry->offset_low = (uint16_t)(addr & 0xFFFF);
+	entry->selector = 0x08;
+	entry->ist = 0;
+	entry->type_attributes = flags;
+	entry->offset_mid = (uint16_t)((addr >> 16) & 0xFFFF);
+	entry->offset_high = (uint32_t)((addr >> 32) & 0xFFFFFFFF);
+	entry->reserved = 0;
+}
+
+void IDT::init() {
+	idtr.limit = sizeof(idt) - 1;
+	idtr.base = (uint64_t)&idt;
+
+	for (int i = 0; i < 256; i++) {
+		set_gate(i, nullptr, 0);
+	}
+
+	load_idt((uint64_t)&idtr);
+}
---
+++ src/idt.h
@@ -0,0 +1,27 @@
+#pragma once 
+#include <stdint>
+
+struct __attribute__((packed)) IDTEntry {
+	uint16_t offset_low;
+	uint16_t selector;
+	uint8_t ist; 
+	uint8_t type_attributes;
+	uint16_t offset_mid;
+	uint32_t offset_high;
+	uint32_t reserved;
+};
+
+struct __attribute__((packed)) IDTR {
+	uint16_t limit;
+	uint64_t base;
+}
+
+class IDT {
+	public: 
+		static IDTEntry idt[256];
+		static IDTR idtr;
+		
+		static void set_gate(uint8_t vector, void* istr, uint8_t flags);
+
+		static viod init();
+};
