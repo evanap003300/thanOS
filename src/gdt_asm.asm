e88a2ef5072db9179808fafb4e664b4dc37cf541
Evan Phillips <evanap003300@gmail.com>
2026-01-08 17:48:25 -0500

    commented gdt

--- src/gdt_asm.asm
+++ src/gdt_asm.asm
@@ -4,15 +4,18 @@ global load_gdt
 
 load_gdt: 
 	lgdt[rdi]
-	mov ax, 0x10
-	mov ds, ax
-	mov es, ax
-	mov fs, ax
-	mov gs, ax
-	mov ss, ax
 
-	pop rdi 
-	mov rax, 0x08
-	push rax 
-	push rdi
-	retfq
+	; Set the permissions for all data related registers 
+
+	mov ax, 0x10       ; 0x10 (2nd byte) => Kernel Data Segment
+	mov ds, ax         ; Data Segment
+	mov es, ax         ; Extra Segment
+	mov fs, ax         ; File Segment
+	mov gs, ax         ; Global Segment
+	mov ss, ax         ; Stack Segment
+
+	pop rdi            ; Store the return address (RIP) in rdi
+	mov rax, 0x08      ; 0x08 (1st byte) => Kernel Code Segment
+	push rax           ; Push 0x08 onto the stack
+	push rdi           ; Push the return address onto the stack
+	retfq              ; Return far quad (long jump back)
