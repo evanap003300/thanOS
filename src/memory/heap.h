ddf4d02bcdce6f058852d8b14fec7e3c686ab284
Evan Phillips <evanap003300@gmail.com>
2026-01-24 23:34:07 -0500

    created heap.h

---
+++ src/memory/heap.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <stdint.h>
+#include <stddef.h> 
+
+struct MemorySegmentHeader {
+	uint64_t length;
+	struct MemorySegmentHeader* next;
+	struct MemorySegmentHeader* last;
+	bool free;	
+
+	void CombineForward();
+	void CombineBackward();
+
+	MemorySegmentHeader* Split(size_t splitLength);
+}
+
+void InitializeHeap(void* heapAddress, size_t pageCount);
+void* malloc(size_t size);
+void free(void* address);
+
+void* operator new(size_t size);
+void* operator new[](size_t size);
+void operator delete(void* p);
+void operator delete[](void* p);
