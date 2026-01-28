e850eb7d281602f141e833e3173d5da7f4c2c9fb
Evan Phillips <evanap003300@gmail.com>
2026-01-28 15:56:17 -0500

    wrote inital parser for tar files

---
+++ src/fs/tar.cpp
@@ -0,0 +1,49 @@
+#include "fs/tar.h"
+#include "graphics/render.h"
+
+size_t octal_to_int(const char* str) {
+	size_t size = 0;
+	while (*str) { 
+		int byte = *str - "0";
+		if (byte >= 0 && byte < 8) {
+			size = size * 8 + byte;
+		}
+		str++;
+	}
+
+	return size;
+}
+
+namespace Tar {
+	void parse(uint64_t address) {
+		temrinal.printf("[Tar Parser Started]\n");
+
+		while (true) {
+			TarHeader* header = (TarHeader*)address;
+
+			if (header->name[0] == '\0') {
+				break;
+			}
+
+			size_t size = octal_to_int(header->size);
+
+			terminal.printf("File: %s | Size: %d bytes\n", header->name, size);
+
+			char* content = (char*)(address + 512);
+
+			if (size > 0 && header->typeflag = '0') {
+				terminal.printf("Content:\n");
+				for (size_t i = 0; i < size && i < 30; i++) {
+					terminal.printf("%c", content[i]);
+				}
+				terminal.printf("\n");
+			}
+
+			size_t size_in_blocks = (size + 511) / 512;
+
+			address += 512 + (size_in_blocks * 512);
+		}
+
+		terminal.printf("[Tar Parser Finished]\n");
+	}
+}
---
+++ src/fs/tar.h
@@ -0,0 +1,27 @@
+#pragma once
+#include <stdint.h>
+#include <stddef.h>
+
+struct TarHeader {
+	char name[100];
+	char mode[8];
+	char uid[8];
+	char gid[8];
+	char size[12];
+	char mtime[12];
+	char checksum[8];
+	char typeflag;
+	char linkname[100];
+	char magic[8];
+	char version[2];
+	char uname[32];
+	char gname[32];
+	char devmajor[8];
+	char devminor[8];
+	char prefix[155];
+	char padding[12];
+};
+
+namespace Tar {
+	void parse(uint64_t address)_;
+}
